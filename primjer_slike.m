df097ab444cb120269f927293313fc87a2649d0b
kgrozdanic <karlo.grozni@gmail.com>
2021-12-15 20:21:14 +0100

    primjer slike ponovno napravljene

--- primjer_slike.m
+++ primjer_slike.m
@@ -1,36 +1,36 @@
 % % % windows logo
-% A = imread('images/windows.png'); W = ImageDist(A, 0.1, 4, 1); % imagesc(W)
-% X = DACA(W, 5, 10); DrawImg(X, A, "DACA, Oklab");
-% X = NC(W, 5); DrawImg(X, A, "NC, Oklab");
+A = imread('images/windows.png'); W = ImageDist(A, 0.1, 4, 1); % imagesc(W)
+X = DACA(W, 5, 10); DrawImg(X, A, "DACA, Oklab");
+X = NC(W, 5); DrawImg(X, A, "NC, Oklab");
 % 
-% A = imread('images/windows.png'); W = ImageDist(A, 0.1, 3, 0); % imagesc(W)
-% X = DACA(W, 5, 5); DrawImg(X, A, "DACA, rgb");
-% X = NC(W, 5); DrawImg(X, A, "NC, rgb");
+A = imread('images/windows.png'); W = ImageDist(A, 0.1, 3, 0); % imagesc(W)
+X = DACA(W, 5, 5); DrawImg(X, A, "DACA, rgb");
+X = NC(W, 5); DrawImg(X, A, "NC, rgb");
 % 
-% A = imread('images/windows.png'); W = ImageDist(A, 0.03, 5, 2); imagesc(W)
-% X = DACA(W, 5, 5); DrawImg(X, A, "DACA, gray");
-% X = NC(W, 5); DrawImg(X, A, "NC, gray");
+A = imread('images/windows.png'); W = ImageDist(A, 0.03, 5, 2);%  imagesc(W)
+X = DACA(W, 5, 5); DrawImg(X, A, "DACA, gray");
+X = NC(W, 5); DrawImg(X, A, "NC, gray");
 
 
 
-% A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.5, 12, 1); imagesc(W)
-% X = DACA(W, 5, 10); DrawImg(X, A, "DACA, Oklab");
-% X = NC(W, 5); DrawImg(X, A, "NC, Oklab");
+A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.5, 12, 1); % imagesc(W)
+X = DACA(W, 5, 10); DrawImg(X, A, "DACA, Oklab");
+X = NC(W, 5); DrawImg(X, A, "NC, Oklab");
 
-% A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.3, 10, 0); imagesc(W)
-% X = DACA(W, 5, 10); DrawImg(X, A, "DACA, rgb");
-% X = NC(W, 5); DrawImg(X, A, "NC, rgb");
+A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.3, 10, 0); % imagesc(W)
+X = DACA(W, 5, 10); DrawImg(X, A, "DACA, rgb");
+X = NC(W, 5); DrawImg(X, A, "NC, rgb");
 
-% A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.2, 10, 2); imagesc(W)
-% X = DACA(W, 5, 10); DrawImg(X, A, "DACA, gray");
-% X = NC(W, 5); DrawImg(X, A, "NC, gray");
+A = imread('images/pablo2.jpg'); W = ImageDist(A, 0.2, 10, 2); % imagesc(W)
+X = DACA(W, 5, 10); DrawImg(X, A, "DACA, gray");
+X = NC(W, 5); DrawImg(X, A, "NC, gray");
 
 
  
 % A = imread('images/kanye_bolji.jpg'); W = ImageDist(A, 0.35, 12, 1); % imagesc(W)
 % X = DACA(W, 7, 10); DrawImg(X, A, "DACA, Oklab");
 % X = NC(W, 5); DrawImg(X, A, "NC, Oklab");
-
+% 
 % A = imread('images/kanye_bolji.jpg'); W = ImageDist(A, 0.1, 12, 0); %imagesc(W)
 % X = DACA(W, 7, 10); DrawImg(X, A, "DACA, rgb");
 % X = NC(W, 5); DrawImg(X, A, "NC, rgb");
