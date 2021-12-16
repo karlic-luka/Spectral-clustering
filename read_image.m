0c0cec84ae62894d831fe41a18aa3e8d6bbb856d
kgrozdanic <karlo.grozni@gmail.com>
2021-12-16 01:59:01 +0100

    novi primjeri, sluzbeni programi

--- read_image.m
+++ read_image.m
@@ -1,12 +1,17 @@
 function I = read_image(file_name, rows)
- 
-I = imread(file_name); %rgb
-[h, w, channels] = size(I);
+    
+    I = imread(file_name); %rgb
+    
+    if rows ~= -1
+        [h, w, channels] = size(I);
 
-%if (channels > 1)
-%    I = double(rgb2gray(I));
-%else
-%    I = double(I);
-%end
+        %if (channels > 1)
+        %    I = double(rgb2gray(I));
+        %else
+        %    I = double(I);
+        %end
 
-I = imresize(I, [rows NaN], 'bicubic');
+        I = imresize(I, [rows NaN], 'bicubic');
+    end
+    
+end
